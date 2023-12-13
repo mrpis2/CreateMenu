82136e53fcc195a76341ba8aeb122a4dac465703
mrpis2 <warhammerprogamer@gmail.com>
2023-12-13 22:05:17 +0700

    createMenu

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+class Program
+{
+    private static void Main(string[] args)
+    {
+        int Choice = 8;
+        while (Choice != 0)
+        {
+            Console.WriteLine("Nhap gia tri");
+            Choice = Convert.ToInt32(Console.ReadLine());
+            switch (Choice)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("********");
+                        Console.WriteLine("********");
+                        Console.WriteLine("********");
+                        Console.WriteLine("********");
+                    }
+                    break;
+                case 2:
+                    {
+                        Console.WriteLine("********");
+                        Console.WriteLine("*******");
+                        Console.WriteLine("******");
+                        Console.WriteLine("*****");
+                        Console.WriteLine("****");
+                        Console.WriteLine("***");
+                        Console.WriteLine("**");
+                        Console.WriteLine("*");
+                    }
+                    break;
+                case 3:
+                    {
+                        Console.WriteLine("********");
+                        Console.WriteLine("********");
+                        Console.WriteLine("********");
+                        Console.WriteLine("********");
+                        Console.WriteLine("********");
+                        Console.WriteLine("********");
+                    }
+                    break;
+                case 0:
+                    {
+                        Environment.Exit(0);
+                    }
+                    break;
+                default:
+                    {
+                        Console.WriteLine("Nhap Lai");
+                    }
+                    break;
+            }
+        }
+      
+
+    }
+}
